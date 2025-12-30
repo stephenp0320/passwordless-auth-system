8f0606c1f226d2f20f1cf5b2b51e8b5b04419c5c
stephenp0320 <stephen power>
2025-12-30 19:33:00 +0000

    WebAuthn registration start endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask   
+from flask import Flask, request, jsonify 
 from flask_cors import CORS
-from fido2.webauthn import PublicKeyCredentialRpEntity
+from fido2.webauthn import PublicKeyCredentialRpEntity, PublicKeyCredentialUserEntity
 from fido2.server import Fido2Server
 
 # https://www.geeksforgeeks.org/python/flask-creating-first-simple-application/
@@ -25,7 +25,26 @@ app.get("/")
 def root():
     return {"message" : "backend is running"}
 
+app.post("/register/start")
+def register_start():
+    username = request.json ["username"]
+    
+    user  = PublicKeyCredentialUserEntity(
+        id = username.encode(),
+        name = username,
+        display_name=username,
+    )
+    
+    options, state = server.register_begin(
+        user,
+        credentials=[],
+        user_verification="preffered",
+    )
+    
+    USERS[username] = user
+    STATES[username] = state
 
+    return jsonify(options)
     
 if __name__ == "__main__":
     app.run(debug=True)
