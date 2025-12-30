1b781a88e97130d6ba7e7b248b3db389fa9f423d
stephenp0320 <stephen power>
2025-12-30 19:21:03 +0000

    initial fido setup

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,22 @@
-from flask import Flask     
+from flask import Flask   
+from flask_cors import CORS
+from fido2.webauthn import PublicKeyCredentialRpEntity
+from fido2.server import Fido2Server
+
 # https://www.geeksforgeeks.org/python/flask-creating-first-simple-application/
 app = Flask(__name__)
+CORS(app)
 
-@app.get('/')
+# fido setup
+responce = PublicKeyCredentialRpEntity(
+    id = "Localhost",
+    name = "Passwordless authenticaiton"
+)
 
-def test_responce():
-    return {"message" : "backend running"}
-    
+server = Fido2Server(responce)
+
     
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
+        
