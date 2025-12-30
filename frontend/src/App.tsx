2510f4ca5dacc204640cba8ffe1511565c498cef
stephenp0320 <stephen power>
2025-12-30 20:30:31 +0000

    passwordless login flow added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import { startRegistration, startAuthentication } from "@simplewebauthn/browser";
 import { useState } from 'react';
-
+//https://simplewebauthn.dev/docs/packages/browser
 function App() {
   const [username, setUsername] = useState("")
+  // passkey registrtion flow
+  //https://simplewebauthn.dev/docs/packages/browser
 
   const register = async () => {
-    const res = await fetch("http://localhost:5000/register/start", {
+    const response = await fetch("http://localhost:5000/register/start", {
       method: "POST",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({ username }),
     });
 
-    const options = await res.json();
+    const options = await response.json();
     const credentials = await startRegistration(options);
     await fetch("http://localhost:5000/register/finish", {
       method: "POST",
@@ -19,7 +21,27 @@ function App() {
       body: JSON.stringify({ username, credentials }),
     });
     alert("Registration Successful!")
-  }
+  };
+  // passwordless login flow
+
+  const login = async () => {
+    const responce = await fetch("http://localhost:5000/login/start", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ username }),
+    });
+
+    const options = await responce.json();
+    const assertion = await startAuthentication(options);
+
+    await fetch("http://localhost:5000/login/finish", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ username , credential: assertion}),
+  });
+  alert("User Login Successful!")
+}
+
   return (
     <>
       <h1>Passwordless Authentiction System</h1>
