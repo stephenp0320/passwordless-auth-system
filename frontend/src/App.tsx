6e9f72db4372cc3323bde0b033459fff783d4c24
stephenp0320 <stephen power>
2025-12-30 20:22:17 +0000

    frontend updated with register passkey flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { startRegistration, startAuthentication } from "@simplewebauthn/browser";
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("")
 
+  const register = async () => {
+    const res = await fetch("http://localhost:5000/register/start", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ username }),
+    });
+
+    const options = await res.json();
+    const credentials = await startRegistration(options);
+    await fetch("http://localhost:5000/register/finish", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ username, credentials }),
+    });
+    alert("Registration Successful!")
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Passwordless Authentiction System</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          it's great becuase it replaces passwords!!!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
 
 export default App
