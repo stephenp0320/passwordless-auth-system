da8a91b5b2e73352864be82ffab6afe3ea1dabe2
stephenp0320 <stephen power>
2026-01-10 19:27:56 +0000

    add fetch_registered_users

---
+++ frontend/src/Admin_dashboard.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+interface User {
+    username: string;
+    registered_at: string;
+    credential_id: string;
+}
+
+function Admin() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [status, setStatus] = useState<{ message: string; type: 'success' | 'error' | '' }>({ message: '', type: '' });
+  
+    // Fetch users on component mount
+    // Reference: https://react.dev/reference/react/useEffect
+    useEffect(() => {
+        fetch_registered_users();
+    }, []);
+
+ // https://www.geeksforgeeks.org/typescript/how-to-use-fetch-in-typescript/
+    const fetch_registered_users = async () => {
+        setIsLoading(true);
+        try {
+            const responce = await fetch("http://localhost:5001/admin/users");
+            if (!responce.ok){
+                throw new Error('failed to fetch registered user');
+            }
+            const data = await responce.json();
+            setUsers(data.users);
+        } catch (error) {
+            console.log('issue retrieving registered user', error)
+            setStatus({ message: 'Failed to load users', type: 'error' });
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+
+
+
+}
