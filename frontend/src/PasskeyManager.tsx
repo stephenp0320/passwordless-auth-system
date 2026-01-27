00e0f187b3489ed4cf770341ab2df9764ea9d382
stephenp0320 <stephen power>
2026-01-27 10:43:38 +0000

    Add fetch_user_passkeys

--- frontend/src/PasskeyManager.tsx
+++ frontend/src/PasskeyManager.tsx
@@ -20,5 +20,27 @@ function PasskeyManager({username} : PasskeyManageProps) {
     useEffect(() => {
         fetch_user_passkeys();
     }, []);
+    
+    // Fetch users passkeys 
+    const fetch_user_passkeys = async () => {
+        setIsLoading(true)
+        try {
+            const response = await fetch("https://localhost:5001/user/passkeys", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username}),
+            })
 
+            if (!response.ok){
+                throw new Error("Failed to fetch users passkeys");
+            }
+            const data = await response.json();
+            setPasskey(data.passkeys);
+        } catch(error) {
+            console.log('Issue fetching passkeys:', error)
+            setStatus({message: "Error loading passkeys", type: "error"})
+        } finally {
+            setIsLoading(false)
+        }
+    };
 }
